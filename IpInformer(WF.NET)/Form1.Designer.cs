236469fc6a16060560c9d83632b81c7acbc98fd0
Anton <xpe6e3@gmail.com>
2023-09-05 18:15:08 +0600

    Add project files.

---
+++ IpInformer(WF.NET)/Form1.Designer.cs
@@ -0,0 +1,328 @@
+﻿namespace IpInformer_WF.NET_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.IpField = new System.Windows.Forms.TextBox();
+            this.startBtn = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.Ip_field = new System.Windows.Forms.Label();
+            this.Ip_lb = new System.Windows.Forms.Label();
+            this.country_lb = new System.Windows.Forms.Label();
+            this.country_field = new System.Windows.Forms.Label();
+            this.city_lb = new System.Windows.Forms.Label();
+            this.city_field = new System.Windows.Forms.Label();
+            this.currency_lb = new System.Windows.Forms.Label();
+            this.currency_field = new System.Windows.Forms.Label();
+            this.anonim_lb = new System.Windows.Forms.Label();
+            this.anonim_field = new System.Windows.Forms.Label();
+            this.vpn_lb = new System.Windows.Forms.Label();
+            this.vpn_field = new System.Windows.Forms.Label();
+            this.proxy_lb = new System.Windows.Forms.Label();
+            this.tor_lb = new System.Windows.Forms.Label();
+            this.proxy_field = new System.Windows.Forms.Label();
+            this.tor_field = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // IpField
+            // 
+            this.IpField.BackColor = System.Drawing.Color.Silver;
+            this.IpField.Location = new System.Drawing.Point(228, 52);
+            this.IpField.Name = "IpField";
+            this.IpField.Size = new System.Drawing.Size(153, 20);
+            this.IpField.TabIndex = 0;
+            // 
+            // startBtn
+            // 
+            this.startBtn.BackColor = System.Drawing.Color.Gray;
+            this.startBtn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.startBtn.Font = new System.Drawing.Font("W3$iP", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startBtn.Location = new System.Drawing.Point(228, 78);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(153, 23);
+            this.startBtn.TabIndex = 1;
+            this.startBtn.Text = "Показать инфо по IP";
+            this.startBtn.UseVisualStyleBackColor = false;
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.outputLabel.Location = new System.Drawing.Point(467, 100);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(313, 320);
+            this.outputLabel.TabIndex = 2;
+            this.outputLabel.Text = "label1";
+            // 
+            // Ip_field
+            // 
+            this.Ip_field.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.Ip_field.Font = new System.Drawing.Font("Disket Mono", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Ip_field.ForeColor = System.Drawing.Color.Yellow;
+            this.Ip_field.Location = new System.Drawing.Point(225, 121);
+            this.Ip_field.Name = "Ip_field";
+            this.Ip_field.Padding = new System.Windows.Forms.Padding(5, 0, 0, 0);
+            this.Ip_field.Size = new System.Drawing.Size(156, 23);
+            this.Ip_field.TabIndex = 3;
+            this.Ip_field.Text = "label1";
+            this.Ip_field.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // Ip_lb
+            // 
+            this.Ip_lb.Font = new System.Drawing.Font("W3$iP", 9.75F);
+            this.Ip_lb.Location = new System.Drawing.Point(65, 120);
+            this.Ip_lb.Name = "Ip_lb";
+            this.Ip_lb.Padding = new System.Windows.Forms.Padding(0, 0, 5, 0);
+            this.Ip_lb.Size = new System.Drawing.Size(154, 23);
+            this.Ip_lb.TabIndex = 2;
+            this.Ip_lb.Text = "IP:";
+            this.Ip_lb.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // country_lb
+            // 
+            this.country_lb.Font = new System.Drawing.Font("W3$iP", 9.75F);
+            this.country_lb.Location = new System.Drawing.Point(65, 154);
+            this.country_lb.Name = "country_lb";
+            this.country_lb.Padding = new System.Windows.Forms.Padding(0, 0, 5, 0);
+            this.country_lb.Size = new System.Drawing.Size(154, 23);
+            this.country_lb.TabIndex = 2;
+            this.country_lb.Text = "Страна:";
+            this.country_lb.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // country_field
+            // 
+            this.country_field.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.country_field.Font = new System.Drawing.Font("Disket Mono", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.country_field.ForeColor = System.Drawing.Color.Yellow;
+            this.country_field.Location = new System.Drawing.Point(225, 155);
+            this.country_field.Name = "country_field";
+            this.country_field.Padding = new System.Windows.Forms.Padding(5, 0, 0, 0);
+            this.country_field.Size = new System.Drawing.Size(156, 23);
+            this.country_field.TabIndex = 3;
+            this.country_field.Text = "label1";
+            this.country_field.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // city_lb
+            // 
+            this.city_lb.Font = new System.Drawing.Font("W3$iP", 9.75F);
+            this.city_lb.Location = new System.Drawing.Point(65, 189);
+            this.city_lb.Name = "city_lb";
+            this.city_lb.Padding = new System.Windows.Forms.Padding(0, 0, 5, 0);
+            this.city_lb.Size = new System.Drawing.Size(154, 23);
+            this.city_lb.TabIndex = 2;
+            this.city_lb.Text = "Город:";
+            this.city_lb.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // city_field
+            // 
+            this.city_field.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.city_field.Font = new System.Drawing.Font("Disket Mono", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.city_field.ForeColor = System.Drawing.Color.Yellow;
+            this.city_field.Location = new System.Drawing.Point(225, 190);
+            this.city_field.Name = "city_field";
+            this.city_field.Padding = new System.Windows.Forms.Padding(5, 0, 0, 0);
+            this.city_field.Size = new System.Drawing.Size(156, 23);
+            this.city_field.TabIndex = 3;
+            this.city_field.Text = "label1";
+            this.city_field.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // currency_lb
+            // 
+            this.currency_lb.Font = new System.Drawing.Font("W3$iP", 9.75F);
+            this.currency_lb.Location = new System.Drawing.Point(65, 223);
+            this.currency_lb.Name = "currency_lb";
+            this.currency_lb.Padding = new System.Windows.Forms.Padding(0, 0, 5, 0);
+            this.currency_lb.Size = new System.Drawing.Size(154, 23);
+            this.currency_lb.TabIndex = 2;
+            this.currency_lb.Text = "Валюта:";
+            this.currency_lb.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // currency_field
+            // 
+            this.currency_field.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.currency_field.Font = new System.Drawing.Font("Disket Mono", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.currency_field.ForeColor = System.Drawing.Color.Yellow;
+            this.currency_field.Location = new System.Drawing.Point(225, 224);
+            this.currency_field.Name = "currency_field";
+            this.currency_field.Padding = new System.Windows.Forms.Padding(5, 0, 0, 0);
+            this.currency_field.Size = new System.Drawing.Size(156, 23);
+            this.currency_field.TabIndex = 3;
+            this.currency_field.Text = "label1";
+            this.currency_field.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // anonim_lb
+            // 
+            this.anonim_lb.Font = new System.Drawing.Font("W3$iP", 9.75F);
+            this.anonim_lb.Location = new System.Drawing.Point(65, 256);
+            this.anonim_lb.Name = "anonim_lb";
+            this.anonim_lb.Padding = new System.Windows.Forms.Padding(0, 0, 5, 0);
+            this.anonim_lb.Size = new System.Drawing.Size(154, 23);
+            this.anonim_lb.TabIndex = 2;
+            this.anonim_lb.Text = "Анонимность:";
+            this.anonim_lb.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // anonim_field
+            // 
+            this.anonim_field.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.anonim_field.Font = new System.Drawing.Font("Disket Mono", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.anonim_field.ForeColor = System.Drawing.Color.Yellow;
+            this.anonim_field.Location = new System.Drawing.Point(225, 257);
+            this.anonim_field.Name = "anonim_field";
+            this.anonim_field.Padding = new System.Windows.Forms.Padding(5, 0, 0, 0);
+            this.anonim_field.Size = new System.Drawing.Size(156, 23);
+            this.anonim_field.TabIndex = 3;
+            this.anonim_field.Text = "label1";
+            this.anonim_field.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // vpn_lb
+            // 
+            this.vpn_lb.Font = new System.Drawing.Font("W3$iP", 9.75F);
+            this.vpn_lb.Location = new System.Drawing.Point(65, 325);
+            this.vpn_lb.Name = "vpn_lb";
+            this.vpn_lb.Padding = new System.Windows.Forms.Padding(0, 0, 5, 0);
+            this.vpn_lb.Size = new System.Drawing.Size(154, 23);
+            this.vpn_lb.TabIndex = 2;
+            this.vpn_lb.Text = "VPN:";
+            this.vpn_lb.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // vpn_field
+            // 
+            this.vpn_field.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.vpn_field.Font = new System.Drawing.Font("Disket Mono", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.vpn_field.ForeColor = System.Drawing.Color.Yellow;
+            this.vpn_field.Location = new System.Drawing.Point(225, 326);
+            this.vpn_field.Name = "vpn_field";
+            this.vpn_field.Padding = new System.Windows.Forms.Padding(5, 0, 0, 0);
+            this.vpn_field.Size = new System.Drawing.Size(156, 23);
+            this.vpn_field.TabIndex = 3;
+            this.vpn_field.Text = "label1";
+            this.vpn_field.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // proxy_lb
+            // 
+            this.proxy_lb.Font = new System.Drawing.Font("W3$iP", 9.75F);
+            this.proxy_lb.Location = new System.Drawing.Point(65, 290);
+            this.proxy_lb.Name = "proxy_lb";
+            this.proxy_lb.Padding = new System.Windows.Forms.Padding(0, 0, 5, 0);
+            this.proxy_lb.Size = new System.Drawing.Size(154, 23);
+            this.proxy_lb.TabIndex = 2;
+            this.proxy_lb.Text = "Прокси:";
+            this.proxy_lb.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // tor_lb
+            // 
+            this.tor_lb.Font = new System.Drawing.Font("W3$iP", 9.75F);
+            this.tor_lb.Location = new System.Drawing.Point(65, 359);
+            this.tor_lb.Name = "tor_lb";
+            this.tor_lb.Padding = new System.Windows.Forms.Padding(0, 0, 5, 0);
+            this.tor_lb.Size = new System.Drawing.Size(154, 23);
+            this.tor_lb.TabIndex = 2;
+            this.tor_lb.Text = "TOR:";
+            this.tor_lb.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // proxy_field
+            // 
+            this.proxy_field.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.proxy_field.Font = new System.Drawing.Font("Disket Mono", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.proxy_field.ForeColor = System.Drawing.Color.Yellow;
+            this.proxy_field.Location = new System.Drawing.Point(225, 291);
+            this.proxy_field.Name = "proxy_field";
+            this.proxy_field.Padding = new System.Windows.Forms.Padding(5, 0, 0, 0);
+            this.proxy_field.Size = new System.Drawing.Size(156, 23);
+            this.proxy_field.TabIndex = 3;
+            this.proxy_field.Text = "label1";
+            this.proxy_field.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // tor_field
+            // 
+            this.tor_field.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.tor_field.Font = new System.Drawing.Font("Disket Mono", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tor_field.ForeColor = System.Drawing.Color.Yellow;
+            this.tor_field.Location = new System.Drawing.Point(225, 360);
+            this.tor_field.Name = "tor_field";
+            this.tor_field.Padding = new System.Windows.Forms.Padding(5, 0, 0, 0);
+            this.tor_field.Size = new System.Drawing.Size(156, 23);
+            this.tor_field.TabIndex = 3;
+            this.tor_field.Text = "label1";
+            this.tor_field.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tor_field);
+            this.Controls.Add(this.currency_field);
+            this.Controls.Add(this.proxy_field);
+            this.Controls.Add(this.country_field);
+            this.Controls.Add(this.tor_lb);
+            this.Controls.Add(this.currency_lb);
+            this.Controls.Add(this.proxy_lb);
+            this.Controls.Add(this.country_lb);
+            this.Controls.Add(this.vpn_field);
+            this.Controls.Add(this.city_field);
+            this.Controls.Add(this.vpn_lb);
+            this.Controls.Add(this.city_lb);
+            this.Controls.Add(this.anonim_field);
+            this.Controls.Add(this.anonim_lb);
+            this.Controls.Add(this.Ip_field);
+            this.Controls.Add(this.Ip_lb);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.startBtn);
+            this.Controls.Add(this.IpField);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox IpField;
+        private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Label Ip_field;
+        private System.Windows.Forms.Label Ip_lb;
+        private System.Windows.Forms.Label country_lb;
+        private System.Windows.Forms.Label country_field;
+        private System.Windows.Forms.Label city_lb;
+        private System.Windows.Forms.Label city_field;
+        private System.Windows.Forms.Label currency_lb;
+        private System.Windows.Forms.Label currency_field;
+        private System.Windows.Forms.Label anonim_lb;
+        private System.Windows.Forms.Label anonim_field;
+        private System.Windows.Forms.Label vpn_lb;
+        private System.Windows.Forms.Label vpn_field;
+        private System.Windows.Forms.Label proxy_lb;
+        private System.Windows.Forms.Label tor_lb;
+        private System.Windows.Forms.Label proxy_field;
+        private System.Windows.Forms.Label tor_field;
+    }
+}
+
---
+++ IpInformer(WF.NET)/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace IpInformer_WF.NET_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            IpField.TextChanged += (s, e) => CheckValidIP(s);
+            startBtn.Click += (s, e) => CheckIp();
+        }
+
+        void CheckValidIP(object component)
+        {
+            if (component is TextBox textBox)
+                if (Regex.IsMatch(textBox.Text, "[^0-9-.]"))
+                {
+                    textBox.Text = textBox.Text.Remove(textBox.TextLength - 1);
+                    textBox.SelectionStart = textBox.TextLength;
+                }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            IpField.TextChanged -= (s, e_) => CheckValidIP(s);
+        }
+
+        async void CheckIp()
+        {
+            string line = "";
+            using (WebClient wc = new WebClient())
+            {
+                line = wc.DownloadString("http://ipwho.is/" + IpField.Text + "?output=xml");
+            }
+            Match match = Regex.Match(line, "<country_code>(.*?)</country_code> ");
+
+            dict["Ip"] = match.Groups[1].Value;
+            dict["Country"] = match.Groups[1].Value;
+            dict["City"] = match.Groups[1].Value;
+            dict["Currency"] = match.Groups[1].Value;
+            dict["Anonim"] = match.Groups[1].Value;
+            dict["Proxy"] = match.Groups[1].Value;
+            dict["VPN"] = match.Groups[1].Value;
+            dict["TOR"] = match.Groups[1].Value;
+            dict["Lat"] = match.Groups[1].Value;
+            dict["Long"] = match.Groups[1].Value;
+
+            outputLabel.Text = line;
+
+        }
+
+        Dictionary<string, string> dict = new Dictionary<string, string>()
+        {
+            { "Ip", ""},
+            { "Country", ""},
+            { "City", ""},
+            { "Currency", ""},
+            { "Anonim", ""},
+            { "Proxy", ""},
+            { "VPN", ""},
+            { "TOR", ""},
+            { "Lat", ""},
+            { "Long", ""},
+
+        };
+
+    }
+}
